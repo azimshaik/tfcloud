a7d444c3773d045a4033f4f08ec72183aa74941d
azimshaik <cheer.buddu@gmail.com>
2021-09-18 17:23:06 -0400

    updated main file

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+ required_version = "1.0.7"
+  backend "remote" {
+    organization = "organizationA1"
+    workspaces {
+      name = "workspace1"
+    }
+  }
+}
+
+variable "name_length" {
+  description = "The number of words in the pet name"
+  default     = "5"
+}
+
+resource "random_pet" "pet_name" {
+  length    = var.name_length
+  separator = "-"
+}
+
+output "pet_name" {
+  value = random_pet.pet_name.id
+}
